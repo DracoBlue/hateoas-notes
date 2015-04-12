4d1c6532a943e0dde13e4edb3c94d8625bdaf60b
DracoBlue <JanS@DracoBlue.de>
2015-04-12 19:32:07 +0200

    Redirect / call to github page

--- src/index.js
+++ src/index.js
@@ -10,4 +10,10 @@ TestData(notes);
 var halApi = require('./api/HalApi');
 app.use('/api/hal', halApi(notes));
 
+app.get('/', function(req, res) {
+	res.statusCode = 302;
+	res.setHeader('Location', 'https://github.com/DracoBlue/hateoas-notes');
+	res.end();
+});
+
 app.listen((process.env.PORT || 3000), (process.env.HOST || '0.0.0.0'));
