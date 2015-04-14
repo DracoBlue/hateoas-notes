3e77872122cec3f701276ed15e99a1bdd953aa6a
DracoBlue <JanS@DracoBlue.de>
2015-04-14 14:42:57 +0200

    HAL: Generate rels for HAL relative to installation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## dev
 
+* generate rels for HAL relative to installation
 * use `cors` npm module for CORS
 * added User, UserAuthentication and Users to domain layer
 * added link for HAL and HTML
--- src/api/HalApi.js
+++ src/api/HalApi.js
@@ -16,16 +16,21 @@ module.exports = function(notes) {
 			var protocol = req.headers['x-forwarded-proto'] || req.protocol;
 			return protocol + '://' + req.hostname + ( port == 80 || port == 443 ? '' : ':' + port ) + req.baseUrl + path;
 		};
+		req.generateVendorRel = function(path) {
+			var urlParts = url.parse(req.protocol + '://' + req.headers['host']);
+			var port = parseInt(req.headers['x-forwarded-port'] ||  urlParts.port, 10);
+			var protocol = req.headers['x-forwarded-proto'] || req.protocol;
+			return protocol + '://' + req.hostname + ( port == 80 || port == 443 ? '' : ':' + port ) + '/rels/' + path
+		};
 		next();
 	});
 
 	api.get('/', function(req, res) {
-		res.send(JSON.stringify({
-			"_links": {
-				"http://hateoas-notes/rels/notes": {"href": req.generateUrl('/notes')},
-				"http://hateoas-notes/rels/create-note": {"href": req.generateUrl('/notes')}
-			}
-		}));
+		var halResponse = {"_links": {}};
+		halResponse["_links"][req.generateVendorRel("notes")] = {"href": req.generateUrl('/notes')};
+		halResponse["_links"][req.generateVendorRel("create-note")] = {"href": req.generateUrl('/notes')};
+
+		res.send(JSON.stringify(halResponse));
 	});
 
 	var noteToHal = function(req, note) {
@@ -54,16 +59,17 @@ module.exports = function(notes) {
 						"self": {"href": req.generateUrl('/notes?offset=' + offset + '&limit=' + limit)},
 						"up": {"href": req.generateUrl("/")},
 						"last": {"href": req.generateUrl('/notes?offset=' + Math.floor(totalCount/limit) + '&limit=' + limit)},
-						"http://hateoas-notes/rels/note": []
 					},
 					"_embedded": {
-						"http://hateoas-notes/rels/note": []
 					}
 				};
 
+				halResponse["_links"][req.generateVendorRel("note")] = [];
+				halResponse["_embedded"][req.generateVendorRel("note")] = [];
+
 				notes.forEach(function(note) {
-					halResponse["_embedded"]["http://hateoas-notes/rels/note"].push(noteToHal(req, note));
-					halResponse["_links"]["http://hateoas-notes/rels/note"].push({"href": req.generateUrl("/notes/" + note.getId())});
+					halResponse["_embedded"][req.generateVendorRel("note")].push(noteToHal(req, note));
+					halResponse["_links"][req.generateVendorRel("note")].push({"href": req.generateUrl("/notes/" + note.getId())});
 				});
 
 				if (hasNextPage)
